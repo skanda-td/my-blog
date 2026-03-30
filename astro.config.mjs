7d8fc3277a6b6a80b87fda32cd6d8c9442e72c55
Skanda <skanda.td@gmail.com>
2026-03-30 16:13:35 +0530

    deploy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://skanda-td.github.io',
+  base: '/my-blog',                      
+
   integrations: [react()],
+
   devToolbar: {
-    enabled: false   // ✅ disables 
+    enabled: false
   }
 });
